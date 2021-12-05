c3553a5c50d2f0f44bb370926436905208aec484
Hacı <94543222+hcdnz@users.noreply.github.com>
2021-12-05 17:34:03 +0300

    Add files via upload

---
+++ Dictionary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Dictionary sınıfı sizin belirleyeceğiniz değişkenlere uygun olarak değerler saklayabilir. 
+             * Değişkeni belirlemek zorunludur. (int,string..)
+            */
+            Dictionary<string, int> Adyas = new Dictionary<string, int>(); // Bu şekilde tanımlanır.
+
+            Adyas.Add("Hacı", 26); // Gibi eleman eklenebilir.
+            Adyas.Add("Büşra", 24);
+            Adyas.Add("Akkoç", 28);
+
+            foreach (var item in Adyas) 
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("---------------------");
+
+            var elemansayisi = Adyas.Count(); // Eleman sayısını yazdırabiliriz.
+            Console.WriteLine("Eleman Sayısı : " + elemansayisi);
+
+            Console.WriteLine("---------------------");
+
+            Adyas.Remove("Akkoç");
+
+            foreach (var item in Adyas)
+            {
+                Console.WriteLine(item);
+            }
+            
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
