ad91f30af6b019b488c1272b37ec59edd9d1ade1
Hacı <94543222+hcdnz@users.noreply.github.com>
2021-12-11 16:37:33 +0300

    Add files via upload

---
+++ OOP2/Individual.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class Individual : Customer // Gerçek Müşteri
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TcNumber { get; set; }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOP2;
+
+Individual customer1 = new Individual();
+customer1.Id = 1;
+customer1.FirstName = "Hacı";
+customer1.LastName = "Deniz";
+customer1.TcNumber = "1234565432";
+customer1.CustomerNumber = "2345432";
+
+Coorporate customer2 = new Coorporate();
+customer2.Id = 2;
+customer2.CustomerNumber = "4242141";
+customer2.CompanyName = "Kodlama.io";
+customer2.TaxNumber = "2345432";
+
+// Customer Individual ve Coorporate'nin referance'lerini tutabiliyor.
+Customer customer3 = new Individual();
+Customer customer4 = new Coorporate();
+
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add(customer1);
+customerManager.Add(customer2);
+customerManager.Add(customer3);
+customerManager.Add(customer4);
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
