e8ebff1f06a54a23b3cc2b0d20133c84b4824a51
Hacı <94543222+hcdnz@users.noreply.github.com>
2021-12-15 14:12:17 +0300

    Add files via upload

---
+++ RecapDemo1/Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.FirstName = "Hacı";
+            customer.City = "Kırşehir";
+
+            Person[] persons = new Person[3] { new Customer { FirstName = "Hacı" },new Student { FirstName = "Büşra" },new Person {FirstName= "Akkoç" } };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+
+    class Customer:Person
+    {
+        public string City { get; set; }
+    }
+
+    class Student:Person
+    {
+        public string Department { get; set; }
+    }
+}
