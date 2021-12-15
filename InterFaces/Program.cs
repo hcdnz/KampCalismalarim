0bcdb82d9b0b596e8a389ef9ef611b006608a0e5
Hacı <94543222+hcdnz@users.noreply.github.com>
2021-12-15 14:44:53 +0300

    Add files via upload

---
+++ InterFaces/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace InterFaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPersonManager customerManager = new CustomerManager(); // İnterface'ler kendisini implemente eden class'ın refarance numarasını tutabilir.
+            customerManager.Add();
+
+            Console.WriteLine("-----------------------------------");
+
+            IPersonManager employeeManager = new EmployeeManager();
+            employeeManager.Add();
+
+            Console.WriteLine("-----------------------------------");
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(customerManager);
+            projectManager.Add(employeeManager);
+            projectManager.Add(new InternManager());
+                                                                      
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IPersonManager 
+        // İnterface'lerde sadece yapılacak işlemin imzası tutulur. İnterface'i impleme eden class'lar içerisinde bulunan işlemleri yapmak zorundadır.
+        // Kısaca İnterface'ler aynı operasyonları faklı şekilde yapan sınıflar için kullanılır.
+        // İnterface'ler new'lenemez. Çünki içinde herhangi bir operasyon tanımlanmaz.
+    {
+        void Add();
+    }
+
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            //Müşteri ekleme kodları 
+            Console.WriteLine("Müşteri Eklendi!");
+        }
+    }
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            // PErsonel ekleme kodları
+            Console.WriteLine("Personel Eklendi!");
+        }
+    }
+    class InternManager : IPersonManager // Gibi daha sonradan bir sınıf eklediğimizde interface ile çalıştığımız için kod düzeni hata vermez.
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer Eklendi!");
+        }
+    }
+
+    class ProjectManager // Tek bir merkezden bütün class'ları yönetebilmek için tanımladık.
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+    }
+}
