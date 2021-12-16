7f2b24b00c1a346d0a91b7736157712553ffc730
Hacı <94543222+hcdnz@users.noreply.github.com>
2021-12-16 15:35:12 +0300

    Add files via upload

---
+++ InterfaceAbstractDemo/Adepters/MernisServiceAdepter.cs
@@ -0,0 +1,21 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using MernisReferanceService;
+using System;
+
+namespace InterfaceAbstractDemo.Adepters
+{
+    public class MernisServiceAdepter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(); 
+            return client.TCKimlikNoDogrula(Convert.ToInt64(customer.NationlityId), customer.FirstName.ToUpper(), customer.LastName.ToUpper(), customer.DateOfBirth.Year);
+        }
+
+        public void Save(Customer customer)
+        {
+            
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Entities/Customer.cs
@@ -0,0 +1,19 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace InterfaceAbstractDemo.Entities
+{
+    public class Customer:IEntity
+    {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public DateTime DateOfBirth { get; set; }
+            public string NationlityId { get; set; }
+        
+    }
+}
+
